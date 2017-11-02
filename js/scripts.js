3a07df91eb14821a57baeabeec0897f4a7283312
Kevin Truong <kevintru0ng@yahoo.com>
2017-11-02 11:35:41 -0700

    added object to hold questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,31 @@
-var quiz = [
-  ["What is Superman's real name?", "Clarke Kent"]
-  ["what is Wonderwoman's real name?", "Dianna Prince"]
-  ["What is Batman's real name?", "Bruce Wayne"]
-];
+quiz = {
+  "name": "Super Hero Name Quiz",
+  "description": "How many super heroes can you name?",
+  "question": "What is the real name of ",
+  "questions":[
+   { "What is Superman's real name?": "Clarke Kent" },
+   { "what is Wonderwoman's real name?": "Dianna Prince" },
+   { "What is Batman's real name?": "Bruce Wayne" }
+]
+}
 var score = 0
 
 play(quiz);
 
 function play(quiz) {
-  var i, quesiton, answer, max;
-  for (i = 0, max = quiz.length; i < max; i++) {
-      question = quiz[i][0];
+  var i, quesiton, answer, max = quiz.length;
+  for (i = 0, i < max; i++) {
+      question = quiz.questions[i].question;
       answer = ask(question);
       check(answer);
   }
   gameOver();
 
   function ask(question) {
-    return prompt(question);
+    return prompt(quiz.question + question);
   }
   function check(answer) {
-    if (answer === quiz[i][1]) {
+    if (answer === quiz.questions[i].answer) {
       alert("Correct!");
       score++;
     } else {
